e8d7edbeedcc3a29cc69c99c2ef28825c61919fc
furimu1234 <enu7693@gmail.com>
2021-02-11 21:15:25 +0900

    通知の処理完成

--- cogs/notice.py
+++ cogs/notice.py
@@ -1,5 +1,8 @@
 from discord.ext import commands as c
+from discord.ext import tasks
+from discord import Embed
 from asyncpgw import general
+from datetime import datetime, timedelta
 
 #pylint: disable=import-error
 from utils import datas, embed, dget
@@ -7,20 +10,55 @@ from utils import datas, embed, dget
 
 bump = """bump(
     server bigint,
-    notice boolean DEFAULT True
+    notice boolean DEFAULT False
     enable boolean DEFAULT True,
     night_enable boolean DEFAULT True,
     mention boolean DEFAULT True,
     night_mention boolean DEFAULT True,
     role bigint,
-    night_role bigint
+    night_role bigint,
+    latest_time timestamp,
+    channel bigint
 )"""
 
+
+BUMP_SUCCESS = 'https://images-ext-1.discordapp.net/external/tAuRcs-FCy2M8OaTS9Ims62J1vrFiviahjBDtpZrrBs/https/disboard.org/images/bot-command-image-bump.png'
+
+#pylint: disable=no-member
 class Notice(c.Cog, name='通知'):
     def __init__(self, bot):
         self.bot = bot
         self.dget = dget.Dget(bot)
         self.bump = general.Pg(bot, 'bump')
+        self.time = general.Pg(bot, 'member_time')
+        self.global_count = general.Pg(bot, 'global_count')
+        self.count_ = general.Pg(bot, 'count')
+        self.disboard = 302050872383242240
+
+        self.check_bump.start()
+
+    
+    def cog_unload(self):
+        self.check_bump.cancel()
+
+    async def count(self, mes):
+        if not (data := await self.global_count.fetch(member=mes.author.id)):
+            await self.global_count.insert(member=mes.author.id)
+
+            data = await self.global_count.fetch(member=mes.author.id)
+
+        await self.global_count.update(count=data['count'] + 1, member=mes.author.id)
+
+        if not (data_ := await self.count_.fetch(server=mes.guild.id, member=mes.author.id)):
+            await self.count_.insert(server=mes.guild.id, member=mes.author.id)
+
+            data_ = await self.count_.fetch(server=mes.guild.id, member=mes.author.id)
+
+        await self.count_.update(count=data['count'] + 1, member=mes.author.id, server=mes.guild.id)
+
+        return data['count'] + 1, data_['count'] + 1
+
+        
 
     async def update_bump(self, ctx, menu, content):
         data = await self.bump.fetch(server=ctx.guild.id)
@@ -105,6 +143,134 @@ class Notice(c.Cog, name='通知'):
         await default_mes.remove_reaction(load_emoji, self.bot.user)
         e = embed.succes(desc='保存に成功したよ！')
         return await ctx.send(embed=e)
+
+
+    @c.Cog.listener()
+    async def on_message(self, mes_bot):
+        if mes_bot.author.id != self.bot.user.id:
+            return
+
+        if not mes_bot.embeds:
+            return
+
+        notice_now = datetime.now()
+
+        for e in mes_bot.embeds:
+            if e.title == Embed.Empty:
+                return
+            
+            if 'ランキングにチャレンジ！' not in e.title:
+                return
+
+        def mes_check(m):
+            return not m.author.bot and m.channel == mes_bot.channel and m.content == '!d bump'
+
+        mes = await self.bot.wait_for('message', check=mes_check)
+
+        if not await self.bump.fetch(server=mes.guild.id, notice=True):
+            return
+        
+        def check(m):
+            return m.author.id == self.disboard and m.channel == mes.channel
+
+        m = await self.bot.wait_for('message', check=check)
+
+        if m.author.id != self.disboard:
+            return
+
+        if not (embed := m.embeds):
+            return
+
+        for e in embed:
+            if e.image.proxy_url != BUMP_SUCCESS:
+                return
+
+        await self.bump.update(latest_time=datetime.now() + timedelta(hours=2), server=mes.guild.id)
+        await self.bump.update(channel=mes.channel.id, server=mes.guild.id)
+
+        now = notice_now - datetime.now()
+        
+        if not await self.time.fetch(member=mes.author.id):
+            await self.time.insert(memebr=mes.author.id)
+
+        await self.time.update(latest_time=now, member=mes.author.id)
+
+        global_count, server_count = await self.count(mes)
+
+        data = await self.time.fetch(member=mes.author.id)
+
+        global_datas = await self.time.sort_not_column_fetchs('latest_time', limit=None)
+
+        server_datas = [data for data in global_datas if mes.guild.get_member(data['member'])]
+
+        data = await self.time.fetch(member=mes.author.id)
+
+        global_rank = global_datas.index(data)
+
+        server_rank = server_datas.index(data)
+
+        e = embed.normal(
+            title = f'BUMPを検知!ランキングに反映!',
+            url = f'https://disboard.org/ja/server/{mes.guild.id}',
+            description = f'BOTの通知が出てからBUMPした時間: {now}\n'\
+                f'サーバーランキング: {server_rank}位\n'\
+                f'グローバルランキング: {global_rank}位\n'\
+                f'貴方がBUMPした回数: {global_count} \n'\
+                f'貴方がこのサーバーでBUMPした回数: {server_count}'
+        )
+        await mes.channel.send(embed=e)
+
+    def times(self, timestamp):
+        d = timestamp.strftime('%d')
+        h = timestamp.strftime('%H')
+        m = timestamp.strftime('%M')
+        s = timestamp.strftime('%S')
+
+        return d, h, m, s
+
+
+    @tasks.loop(seconds=1)
+    async def check_bump(self):
+        await self.bot.wait_until_ready()
+        now = datetime.now()
+
+        for guild in self.bot.guilds:
+
+            role = None
+
+            if not (data := await self.bump.fetch(server=guild.id)): continue
+
+            if not (channel := guild.get_channel(data['channel'])): continue
+            
+            if now.strftime('%H') in ['00', '01', '02', '03', '04', '05', '06']:
+                if not data['night_enable']: continue
+                
+                if data['night_mention']:
+                    role = guild.get_role(data['night_role'])
+
+            else:
+                if not data['enable']: continue
+
+                if data['mention']:
+                    role = guild.get_role(data['role'])
+     
+            data_day, data_hour, data_minute, data_second = self.times(data['latest_time'])
+            day, hour, minute, second= self.times(now)
+
+            if data_day != day: continue
+            if data_hour != hour: continue
+            if data_minute != minute: continue
+            if data_second != second: continue
+
+            e = embed.normal(
+                title = 'BUMPできるよ！ランキングにチャレンジ！',
+            )
+
+            if not role:
+                await channel.send(embed=e)
+            else:
+                await channel.send(role.mention, embed=e)
+                        
 
 
 def setup(bot):
